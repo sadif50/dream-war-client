76f8f4d7fccb876dd96db07870a198e79eb9afce
sadif50 <smsujon50@gmail.com>
2022-10-26 14:01:41 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello From App
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEV0-DYzFtDkYJSPqA9HS7m4B3eMwJ_lM",
+  authDomain: "dream-war-602c1.firebaseapp.com",
+  projectId: "dream-war-602c1",
+  storageBucket: "dream-war-602c1.appspot.com",
+  messagingSenderId: "386525802424",
+  appId: "1:386525802424:web:e5b8bdfe880877acafe94d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
