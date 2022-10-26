fee9a06a875358933aeb1b76adb6481498890ff8
sadif50 <smsujon50@gmail.com>
2022-10-27 04:27:55 +0600

    Login and registration form design complete

--- src/Pages/User/Login/Login.js
+++ src/Pages/User/Login/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            LogIn
+        <div className='login_container secondary-bg'>
+            <h2 className='text-center'>PLEASE LOG IN</h2>
+            <Form className='mt-4'>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
+                </Form.Group>
+                <Button className='route_item m-0 secondary-bg' type="submit">
+                    LOG IN
+                </Button>
+            </Form>
+            <h5 className='mt-3'>New to here? Please <Link to='/register'>Register</Link></h5>
         </div>
     );
 };
--- src/Pages/User/Register/Register.js
+++ src/Pages/User/Register/Register.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className='login_container secondary-bg'>
+            <h2 className='text-center'>REGISTRATION FORM</h2>
+            <Form className='mt-4'>
+                <Form.Group className="mb-3">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" name='fullName' placeholder="Enter full name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Photo Url</Form.Label>
+                    <Form.Control type="text" name='photoURL' placeholder="Enter photo url" />
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter email" required/>
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder="Password" required/>
+                </Form.Group>
+                <Button className='route_item m-0 secondary-bg' type="submit">
+                    REGISTER
+                </Button>
+            </Form>
+            <h5 className='mt-3'>Already Have An Account? Please <Link to='/login'>Login</Link></h5>
         </div>
     );
 };
